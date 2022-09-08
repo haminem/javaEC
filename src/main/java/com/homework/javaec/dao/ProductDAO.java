07862750f46b424ee6d5797976a2d509bae40cb8
ryunosuke_sako <ryuu.shushu@gmail.com>
2022-09-08 18:35:05 +0900

    update ProductDAOに全レコード取得機能を追加

--- src/main/java/com/homework/javaec/dao/ProductDAO.java
+++ src/main/java/com/homework/javaec/dao/ProductDAO.java
@@ -1,5 +1,13 @@
 package com.homework.javaec.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.homework.javaec.bean.ProductBean;
 
 public class ProductDAO {
@@ -13,6 +21,26 @@ public class ProductDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             throw new DAOException("ドライバの登録に失敗しました。");
+        }
+    }
+    
+    public List<ProductBean> findAll() throws DAOException {
+        String sql = "SELECT * FROM product";
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement st = con.prepareStatement(sql);
+                ResultSet rs = st.executeQuery()) {
+            List<ProductBean> list = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                ProductBean product = new ProductBean(id, name, price);
+                list.add(product);
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DAOException("レコードの取得に失敗しました。");
         }
     }
 }
